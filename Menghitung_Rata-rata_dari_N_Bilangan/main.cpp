b3232fd6312e4550b45f7b201e2e28037a4c3dbb
vicaa <susantoludevica@gmail.com>
2025-10-28 09:55:06 +0700

    tambah hitung rata_rata

--- Menghitung_Rata-rata_dari_N_Bilangan/main.cpp
+++ Menghitung_Rata-rata_dari_N_Bilangan/main.cpp
@@ -1,1 +1,21 @@
-// Codenya taruh sini dan juga jangan lupa compile berupa a.out 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    float bilangan, total = 0, rata;
+
+    cout << "Masukkan jumlah bilangan (N): ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cout << "Masukkan bilangan ke-" << i << ": ";
+        cin >> bilangan;
+        total += bilangan;
+    }
+
+    rata = total / n;
+    cout << "Rata-rata dari " << n << " bilangan adalah: " << rata << endl;
+
+    return 0;
+}
